74fb4c8c252713af4ef9ff881a30a87c647be6d5
Lorenzo Fontoura <lorenzo@nioel.com>
2020-01-07 01:53:15 +1000

    Add RotationAxis

---
+++ Runtime/Scripts/RotationAxis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using Object = System.Object;
+
+namespace TransformManipulation {
+
+    public struct RotationAxis {
+        private readonly Vector3 forwardLocal;
+        private readonly Vector3 upLocal;
+        private readonly Transform target;
+
+        public Vector3 Up => Vector3.Cross(Forward, TargetDirection(this.upLocal));
+        public Vector3 Forward => TargetDirection(this.forwardLocal);
+        public Transform Target => this.target;
+
+        private Vector3 TargetDirection(Vector3 direction) => this.target.TransformDirection(direction);
+
+        public RotationAxis(Transform target, Vector3 forward, Vector3 up) {
+            this.target = target;
+            this.upLocal = up;
+            this.forwardLocal = forward;
+        }
+    }
+}
